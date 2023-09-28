237251577182221b35e161e1f3224a5786b3430a
atulsaini7900 <atulsaini7900@gmail.com>
2023-09-28 08:35:05 +0530

    create Totoitem.js file

---
+++ src/components/TodoItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Icon } from 'react-icons-kit';
+import { edit2 } from 'react-icons-kit/feather/edit2';
+import { trash } from 'react-icons-kit/feather/trash';
+
+const TodoItem = ({ todo, onEdit, onDelete, onCheckboxChange }) => {
+  return (
+    <div className='todo' style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+      <div>
+        <input
+          type='checkbox'
+          checked={todo.completed}
+          onChange={() => onCheckboxChange(todo.ID)}
+          style={{ marginRight: '10px' }}
+        />
+        <span style={{ flex: '1', fontSize: '16px', textDecoration: todo.completed === true ? 'line-through' : 'none' }}>
+          {todo.TodoValue}
+        </span>
+      </div>
+
+      <div className='edit-and-delete' style={{ display: 'flex', alignItems: 'center', marginLeft: '10px' }}>
+        <div style={{ cursor: 'pointer', marginLeft: '5px' }} onClick={() => onEdit(todo)}>
+          <Icon icon={edit2} size={18} />
+        </div>
+        <div style={{ cursor: 'pointer', marginLeft: '5px' }} onClick={() => onDelete(todo.ID)}>
+          <Icon icon={trash} size={18} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoItem;
